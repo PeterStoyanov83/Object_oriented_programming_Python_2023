3277cc01f0880bf67a4b403e53a45f1289aa8dc0
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-06-22 10:42:51 +0200

    studying...

---
+++ 01_First_Steps_In_OOP/00_presentation.py
@@ -0,0 +1,17 @@
+def print_list(list_args):
+    for el in list_args:
+        if el is not None:
+            print(el)
+
+
+list1 = [1, 2, None, 3]
+
+# for el in list1:
+#     print(el)
+print_list(list1)
+
+list2 = [1, 2, 3, 4]
+
+# for el in list2:
+#     print(el)
+print_list(list2)
