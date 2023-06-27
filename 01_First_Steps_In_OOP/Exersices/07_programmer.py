e775ba8470b6da0e265f34f9fe4bd78c2abb1768
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-06-27 13:06:05 +0200

    studying...

--- 01_First_Steps_In_OOP/Exersices/07_programmer.py
+++ 01_First_Steps_In_OOP/Exersices/07_programmer.py
@@ -4,20 +4,22 @@ class Programmer:
         self.language = language
         self.skills = skills
 
-    def watch_course(self, course_name, language, skills_earned):
-        if self.language == language:
-            self.skills += skills_earned
-            return f"{self.name} watched {course_name}"
-        return f"{self.name} does not know {language}"
+    def watch_course(self, course_name: str, language: str, skills_earned: int) -> str:
+        if self.language != language:
+            return f"{self.name} does not know {language}"
+        self.skills += skills_earned
+        return f"{self.name} watched {course_name}"
 
-    def change_language(self, new_language, skills_needed):
-        if self.skills > skills_needed:
-            self.language = new_language
-            return f"{self.name} switched from {self.language} to {new_language}"
-        elif self.skills == skills_needed:
-            self.language = new_language
+    def change_language(self, new_language: str, skills_needed: int) -> str:
+        if self.skills >= skills_needed:
+            if self.language != new_language:
+                previous_language = self.language
+                self.language = new_language
+
+                return f"{self.name} switched from {previous_language} to {new_language}"
             return f"{self.name} already knows {new_language}"
-        return f"{self.name} needs {abs(self.skills - skills_needed)} more skills"
+
+        return f"{self.name} needs {skills_needed - self.skills} more skills"
 
 
 programmer = Programmer("John", "Java", 50)
