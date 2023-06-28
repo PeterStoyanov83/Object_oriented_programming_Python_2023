051fd0a3216da10cdf9269e784d2aa22b7359d87
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-06-28 14:20:50 +0200

    studying...

--- 02_Classes_and_Objects /exersices /05_ToDoList/project/section.py
+++ 02_Classes_and_Objects /exersices /05_ToDoList/project/section.py
@@ -32,16 +32,16 @@ class Section:
             section_info += f"\n{task.details()}"
         return section_info
 
-
-task = Task("Make bed", "27/05/2020")
-print(task.change_name("Go to University"))
-print(task.change_due_date("28.05.2020"))
-task.add_comment("Don't forget laptop")
-print(task.edit_comment(0, "Don't forget laptop and notebook"))
-print(task.details())
-section = Section("Daily tasks")
-print(section.add_task(task))
-second_task = Task("Make bed", "27/05/2020")
-section.add_task(second_task)
-print(section.clean_section())
-print(section.view_section())
+#
+# task = Task("Make bed", "27/05/2020")
+# print(task.change_name("Go to University"))
+# print(task.change_due_date("28.05.2020"))
+# task.add_comment("Don't forget laptop")
+# print(task.edit_comment(0, "Don't forget laptop and notebook"))
+# print(task.details())
+# section = Section("Daily tasks")
+# print(section.add_task(task))
+# second_task = Task("Make bed", "27/05/2020")
+# section.add_task(second_task)
+# print(section.clean_section())
+# print(section.view_section())
