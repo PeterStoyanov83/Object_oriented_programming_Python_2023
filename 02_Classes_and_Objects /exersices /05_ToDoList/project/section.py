efc22d954b800a98ac99e1b8f37359feb588c78b
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-06-28 14:09:24 +0200

    studying...

--- 02_Classes_and_Objects /exersices /05_ToDoList/project/section.py
+++ 02_Classes_and_Objects /exersices /05_ToDoList/project/section.py
@@ -1,7 +1,11 @@
+from typing import List
+from project.task import Task
+
+
 class Section:
     def __init__(self, name: str):
         self.name = name
-        self.tasks = []
+        self.tasks: List = []
 
     def add_task(self, new_task: 'Task') -> str:
         if new_task in self.tasks:
@@ -27,3 +31,17 @@ class Section:
         for task in self.tasks:
             section_info += f"\n{task.details()}"
         return section_info
+
+
+# task = Task("Make bed", "27/05/2020")
+# print(task.change_name("Go to University"))
+# print(task.change_due_date("28.05.2020"))
+# task.add_comment("Don't forget laptop")
+# print(task.edit_comment(0, "Don't forget laptop and notebook"))
+# print(task.details())
+# section = Section("Daily tasks")
+# print(section.add_task(task))
+# second_task = Task("Make bed", "27/05/2020")
+# section.add_task(second_task)
+# print(section.clean_section())
+# print(section.view_section())
