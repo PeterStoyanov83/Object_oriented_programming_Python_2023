dd28df6ce02ba9de358efecba6066313b56a7ee3
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-07-13 16:23:38 +0300

    studying...

--- 04_Encapsulation/exersice /02_Pizza_maker/project/dough.py
+++ 04_Encapsulation/exersice /02_Pizza_maker/project/dough.py
@@ -14,3 +14,24 @@ class Dough:
             raise ValueError("The flour type cannot be an empty string")
         self.__flour_type = value
 
+    @property
+    def baking_technique(self):
+        return self.__baking_technique
+
+    @baking_technique.setter
+    def baking_technique(self, value: str) -> None:
+        if value == "":
+            raise ValueError("The baking technique cannot be an empty string")
+        self.__baking_technique = value
+        
+    @property
+    def weight(self):
+        return self.__weight
+    
+    @weight.setter
+    def weight(self, value):
+        if value <= 0:
+            raise ValueError("The weight cannot be less or equal to zero")
+        self.__weight = value
+
+    
