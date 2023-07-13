cade303cb90e7967c35c85a06a153c6ad08e7b4c
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-07-13 18:12:17 +0300

    studying...

--- 04_Encapsulation/exersice /04_restaurant/project/beverage/tea.py
+++
@@ -1,9 +0,0 @@
-from project.beverage.hot_beverage import HotBeverage
-
-
-class Tea(HotBeverage):
-    MILLILITERS = 50
-    PRICE = 3.50
-
-    def __init__(self):
-        super().__init__("Tea", Tea.PRICE, Tea.MILLILITERS)
