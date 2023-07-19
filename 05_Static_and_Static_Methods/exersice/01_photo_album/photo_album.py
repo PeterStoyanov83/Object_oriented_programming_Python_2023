bb36b2b0469c02cad62d54a70287cac4d8a44577
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-07-19 12:15:14 +0200

    studying...

--- 05_Static_and_Static_Methods/exersice/01_photo_album/photo_album.py
+++ 05_Static_and_Static_Methods/exersice/01_photo_album/photo_album.py
@@ -1,9 +1,11 @@
-from typing import List
-from math import ceil
+from typing import List, TypeVar
+import math
+
+T = TypeVar("T", bound="TrivialClass")
 
 
 class PhotoAlbum:
-    def __init__(self, pages: int):
+    def __init__(self, pages: int) -> None:
         self.pages = pages
         self.photos: List[List[str]] = self.__initialize_photos()
         self.current_row_index = 0  # acessing 1st elment in the matrix
@@ -15,11 +17,11 @@ class PhotoAlbum:
         return matrix
 
     @classmethod
-    def _from_photos_count(cls, photos_count: int):
-        pages = ceil(photos_count / 4)
+    def _from_photos_count(cls, photos_count: int) -> T:
+        pages = math.ceil(photos_count / 4)
         return cls(pages)
 
-    def add_photo(self, label: str):
+    def add_photo(self, label: str) -> str:
         if len(self.photos[self.current_row_index]) == 4:
             self.current_row_index += 1
         try:
@@ -31,8 +33,16 @@ class PhotoAlbum:
         except IndexError:
             return "No more free slots"
 
-    def display(self):
-        pass
+    def display(self) -> str:
+        res = ""
+        for page in self.photos:
+            if len(page) == 0:
+                res += "-" * 11 + "\n"
+            else:
+                res += "-" * 11 + "\n"
+                res += " ".join(["[]" for _ in page]) + "\n"
+        res += "-" * 11
+        return res
 
 
 album = PhotoAlbum(2)
@@ -40,7 +50,6 @@ print(album.add_photo("baby"))
 print(album.add_photo("first grade"))
 print(album.add_photo("eight grade"))
 print(album.add_photo("party with friends"))
-
 print(album.photos)
 print(album.add_photo("prom"))
 print(album.add_photo("wedding"))
