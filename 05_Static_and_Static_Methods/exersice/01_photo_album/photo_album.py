827e766993b3a24c04f2df3469e200af9a773e0f
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-07-20 11:17:46 +0200

    studying...

--- 05_Static_and_Static_Methods/exersice/01_photo_album/photo_album.py
+++ 05_Static_and_Static_Methods/exersice/01_photo_album/photo_album.py
@@ -1,46 +1,26 @@
-from typing import List, TypeVar
-import math
-
-T = TypeVar("T", bound="TrivialClass")
-
-
 class PhotoAlbum:
-    def __init__(self, pages: int) -> None:
+    def __init__(self, pages: int):
         self.pages = pages
-        self.photos: List[List[str]] = self.__initialize_photos()
-        self.current_row_index = 0  # acessing 1st elment in the matrix
-
-    def __initialize_photos(self) -> List[List[str]]:
-        matrix = []
-        for _ in range(self.pages):
-            matrix.append([])
-        return matrix
+        self.photos = [[] for _ in range(pages)]
 
     @classmethod
-    def _from_photos_count(cls, photos_count: int) -> T:
-        pages = math.ceil(photos_count / 4)
+    def from_photos_count(cls, photos_count: int) -> 'PhotoAlbum':
+        pages = (photos_count + 3) // 4
         return cls(pages)
 
     def add_photo(self, label: str) -> str:
-        if len(self.photos[self.current_row_index]) == 4:
-            self.current_row_index += 1
-        try:
-
-            self.photos[self.current_row_index].append(label)
-            return f"{label} photo added successfully on page " \
-                   f"{self.current_row_index + 1} " \
-                   f"slot {len(self.photos[self.current_row_index])}"
-        except IndexError:
-            return "No more free slots"
+        for page_number, page in enumerate(self.photos, start=1):
+            if len(page) < 4:
+                slot_number = len(page) + 1
+                page.append(label)
+                return f"{label} photo added successfully on page {page_number} slot {slot_number}"
+        return "No more free slots"
 
     def display(self) -> str:
         res = ""
         for page in self.photos:
-            if len(page) == 0:
-                res += "-" * 11 + "\n"
-            else:
-                res += "-" * 11 + "\n"
-                res += " ".join(["[]" for _ in page]) + "\n"
+            res += "-" * 11 + "\n"
+            res += " ".join(["[]" for _ in page]) + "\n"
         res += "-" * 11
         return res
 
