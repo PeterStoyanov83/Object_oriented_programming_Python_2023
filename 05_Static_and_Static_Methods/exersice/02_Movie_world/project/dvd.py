81ccd119d035729def521f3c413077874754f07d
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-07-20 13:49:19 +0200

    studying...

---
+++ 05_Static_and_Static_Methods/exersice/02_Movie_world/project/dvd.py
@@ -0,0 +1,19 @@
+class DVD:
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    def __init__(self, name: str, id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
+        day, month, year = map(int, date.split('.'))
+        return cls(name, id, year, cls.months[month-1], age_restriction)
+
+    def __repr__(self):
+        status = "rented" if self.is_rented else "not rented"
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction {self.age_restriction}. Status: {status}"
