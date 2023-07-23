4eefa599b66cb84dcf7649052138ac402e39bbd3
Petar Stoyanov <peterstoyanov83@gmail.com>
2023-07-23 13:02:55 +0200

    studying...

--- 05_Static_and_Static_Methods/06_abstractions/exercises/06_*_Formula_1_manager/project/formula_teams/formula_team.py
+++ 05_Static_and_Static_Methods/06_abstractions/exercises/06_*_Formula_1_manager/project/formula_teams/formula_team.py
@@ -1,9 +1,20 @@
 from abc import ABC, abstractmethod
+from typing import Dict
 
 
 class FormulaTeam(ABC):
     def __init__(self, budget: int):
         self.budget = budget
+
+    @property
+    @abstractmethod
+    def sponsors(self) -> Dict[str, Dict[int, int]]:
+        ...
+
+    @property
+    @abstractmethod
+    def expenses(self) -> int:
+        ...
 
     @property
     def budget(self):
@@ -16,8 +27,16 @@ class FormulaTeam(ABC):
 
         self.__budget = value
 
-    @abstractmethod
-    def calculate_revenue_after_race(self,race_pos:int):
-        ...
+    def calculate_revenue_after_race(self, race_pos: int) -> str:
+        revenue = 0
 
+        for positions in self.sponsors.values():  #  {1: 1_500_000, 2: 800_000}
+            for pos in positions:
+                if race_pos <= pos:
+                    revenue += positions[pos]
+                    break
 
+        revenue -= self.expenses
+        self.budget += revenue
+
+        return f"The revenue after the race is {revenue}$. Current budget {self.budget}$"
