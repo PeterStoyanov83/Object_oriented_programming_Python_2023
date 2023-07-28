e1492f06d626262731be49843d95411f545e2ccc
peterstoyanov <peterstoyanov83@gmail.com>
2023-07-28 14:37:54 +0200

    studying...

---
+++ 08_iterators_Generators/Excercises/01_take_skip.py
@@ -0,0 +1,16 @@
+class take_skip:  # the reason of smaller letters because it is USED AS FUNCTION
+    def __init__(self, step: int, count: int):
+        self.step = step
+        self.count = count
+        self.iterations = -1  # so we don't change count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.iterations == self.count - 1:
+            raise StopIteration
+
+        self.iterations += 1
+
+        return self.iterations * self.step
