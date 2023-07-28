3e7b7127c4f484db575126fcb2a508084be023ae
peterstoyanov <peterstoyanov83@gmail.com>
2023-07-28 18:19:25 +0200

    studying...

---
+++ 08_iterators_Generators/Excercises/08_prime_numbers.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+
+def get_primes(numbers: list):
+    for number in numbers:
+        if number <= 1:
+            continue
+
+        for divisor in range(2, int(sqrt(number)) + 1):
+            if number % divisor == 0:
+                break
+
+        else:
+            yield number
+
+
+print(list(get_primes([100_000_007, 4, 3, 5, 6, 9, 1, 0])))
+print(list(get_primes([-2, 0, 0, 1, 1, 0])))
---
+++ 08_iterators_Generators/Excercises/09_possible_permutations.py
@@ -0,0 +1,6 @@
+from itertools import permutations
+
+
+def possible_permutations(sequence):
+    for el in (permutations(sequence)):
+        yield list(el)
